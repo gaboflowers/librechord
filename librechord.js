df348ed1e607d8b0baa4c421fa8c003ffbe0eb86
Gabriel Flores <gabriel.flores.m@ug.uchile.cl>
2022-11-10 03:55:06 -0300

    Support for ==Named stanzas==

--- librechord.js
+++ librechord.js
@@ -9,7 +9,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License
- * along with LibreChord.  If not, see <https://www.gnu.org/licenses/>.
+ * along with Covertau.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 /*
@@ -25,6 +25,7 @@ function HTMLDecode(s) {
   return s.replace(/&lt;/g,'<').replace(/&gt;/g,'>').replace(/&amp;/g, '&');
 }
 
+const newDecodedStanza = function () { return {'lines': [], 'name': null}; }
 
 function LChorder(data, target, options) {
   if (options === undefined) {
@@ -37,7 +38,8 @@ function LChorder(data, target, options) {
   this.rawLines = this.rawText.split('\n').map(HTMLDecode);
   this.decodedSong = [];
   this.isDecoded = false;
-  this.lastDecodedStanza = [];
+  this.nextStanzaName = null;
+  this.lastDecodedStanza = newDecodedStanza();
   this.doubleNewLine = false;
   this.attachedChords = {};
   this.chordDefinitions = options.chordDefinitions;
@@ -63,12 +65,25 @@ function LChorder(data, target, options) {
       throw 'No hay ninguna canción decodificada';
     }
 
-    node.innerHTML = '';
+    node.innerHTML = ''
     this.decodedSong.forEach(function(stanza) {
       let divStanza = document.createElement('div');
       divStanza.classList.add('lch-stanza');
 
-      stanza.forEach(function(line) {
+      if (stanza.name) {
+        let tableTitle = document.createElement('table');
+        tableTitle.classList.add('lch-line');
+        tableTitle.classList.add('lch-stanza-name');
+
+        let rowTitle = document.createElement('tr');
+        let dataTitle = document.createElement('td');
+        dataTitle.innerText = stanza.name;
+        rowTitle.append(dataTitle);
+        tableTitle.append(rowTitle);
+        divStanza.append(tableTitle);
+      }
+
+      stanza.lines.forEach(function(line) {
         let tableLine = document.createElement('table');
         tableLine.classList.add('lch-line');
 
@@ -77,15 +92,26 @@ function LChorder(data, target, options) {
         rowChords.classList.add('lch-chords');
         rowText.classList.add('lch-text');
 
-        line.forEach(function(symbol) {
+        line.symbols.forEach(function(symbol) {
           let dataChord = document.createElement('td');
           let dataText = document.createElement('td');
           dataChord.innerHTML = '<a href="#">'+symbol.chord+'</a>';
-          dataText.innerHTML = symbol.text.replace(/ /g, '&nbsp;');
+          textNodeDataText = document.createTextNode(
+                                      symbol.text.replace(/ /g, '\u00A0')); // &nbsp;
+          dataText.innerHTML = '';
+          dataText.append(textNodeDataText);
 
           rowChords.appendChild(dataChord);
           rowText.appendChild(dataText);
         });
+        if (line.note) {
+          rowChords.lastChild.classList.add('lch-chord-prev-to-note');
+          let dataNote = document.createElement('td');
+          dataNote.classList.add('lch-chord-note');
+          dataNote.innerText = line.note;
+          rowChords.appendChild(dataNote);
+        }
+
         tableLine.appendChild(rowChords);
         tableLine.appendChild(rowText);
 
@@ -105,6 +131,25 @@ function LChorder(data, target, options) {
   }
 
   this.decode = function () {
+    /* this.decode transforma las líneas leídas en this.rawLines en una canción
+     * parseada en this.decodedSong
+     *
+     * ORIGINAL:
+     * this.decodedSong contiene un arreglo de estrofas ("stanza")
+     * cada estrofa es un arreglo de versos ("line")
+     * cada verso es un arreglo de símbolos (objetos con campo 'chord' y 'text')
+     *
+     * NUEVA VERSIÓN:
+     * TODO una canción (this.decodedSong) es un objeto con:
+     *   - un campo 'stanzas' (que contiene un arreglo de estrofas)
+     *   - otros campos, como 'title', 'artist', 'references'
+     * cada estrofa es un objeto con:
+     *   - un campo 'lines' (que contiene un arreglo de versos)
+     *   - un campo 'name' (que contiene un string)
+     * TODO cada verso es un objeto con:
+     *   - un campo 'symbols' (que contiene un arreglo de símbolos)
+     *   - un campo 'note' (que contiene un string)
+     * */
     for (let i=0; i<this.rawLines.length; i++) {
       let thisDecodedLine = [];
 
@@ -112,9 +157,12 @@ function LChorder(data, target, options) {
       if (thisLine.length == 0) {
         // dos saltos de línea marcan una nueva estrofa
         if (this.doubleNewLine) {
-          if (this.lastDecodedStanza.length > 0 ){
+          if (this.lastDecodedStanza.lines.length > 0) {
+            this.lastDecodedStanza.name = this.nextStanzaName;
+            this.nextStanzaName = null;
+            // Aquí se agrega una nueva estrofa a la canción
             this.decodedSong.push(this.lastDecodedStanza);
-            this.lastDecodedStanza = [];
+            this.lastDecodedStanza = newDecodedStanza();
           }
           this.doubleNewLine = false;
         } else {
@@ -128,33 +176,55 @@ function LChorder(data, target, options) {
         continue;
       }
 
+      if (thisLine[0] == '=') {
+        // Esta línea tiene el título de una estrofa
+        this.nextStanzaName = thisLine.substr(1).replace(/^=+/, '').replace(/=+$/, '');
+        this.nextStanzaName = this.nextStanzaName.replace(/^\s+/, '').replace(/\s+$/, '');
+        continue;
+      }
+
       if (thisLine[0] != '<') {
         // Estoy esperando una línea que contenga una secuencia de acordes
         continue;
       }
+      this.doubleNewLine = false; // ya estamos en la nueva estrofa
+      // thisLine tiene una secuencia de acordes
 
-      this.doubleNewLine = false;
-
+      /*
+       * // Ahora acepto secuencias de acordes sueltas:
+       * //
+       * // <-Dm-----|E7-----|Am-
+       * //
       if (i+2 >= this.rawLines.length) {
         throw 'Fin de archivo LibreChord inesperado';
       }
+      */
 
-      let rawChordLine = thisLine;
+      //let rawChordSequence = thisLine;
+      //  Ahora acepto notas al final de una secuencia de acordes
+      //  <-A-|-C#m-|-Bm-|-D-(x3)
+      let rawChordToks = thisLine.split(' ')
+      let rawChordSequence = rawChordToks[0];
+      // La parte de las notas 
+      let rawLineNotesPart = rawChordToks.slice(1);
+      let rawLineNotes = rawLineNotesPart ? rawLineNotesPart.reduce((a,b) => a.replace(/ +$/,'') + ' ' + b.replace(/ +$/,''), '') :
+                                            null;
       let middleLine = this.rawLines[i+1];
       let rawTextLine = this.rawLines[i+2];
+
+      let freeChordSequence = false;
       if (middleLine[0] != '+') {
-        throw 'Se esperaba \'+\', se encontró \''+middleLine[0]+'\'';
-      }
-      if (rawTextLine[0] != '>') {
-        throw 'Se esperaba \'>\', se encontró \''+middleLine[0]+'\'';
+        // secuencia de acorde suelta
+        freeChordSequence = true;
+      } else if (rawTextLine[0] != '>') {
+        throw `Se esperaba '>', se encontró '${rawTextLine[0]}'`;
       }
 
-      rawChordLine = rawChordLine.substr(1).replace(/-/g, '');
       middleLine = middleLine.substr(1).replace(/-/g, '');
       rawTextLine = rawTextLine.substr(1).replace(/-/g, '');
 
-      let chords = rawChordLine.replace(/ +/g, '').split('|');
-      let morae = rawTextLine.split('|');
+      let chords = this.parseChordSequence(rawChordSequence);
+      let morae = freeChordSequence ? new Array(chords.length).fill('   ') : rawTextLine.split('|');
       if (chords.length != morae.length) {
         throw `La cantidad de acordes es ${chords.length}, distinta del largo de la cantidad de moras ${morae.length}. Revisa tus separa|dores.`
       }
@@ -173,26 +243,35 @@ function LChorder(data, target, options) {
                          'text': morae[j]};
         thisDecodedLine.push(newSymbol);
       }
-      this.lastDecodedStanza.push(thisDecodedLine);
+      this.lastDecodedStanza.lines.push({'symbols': thisDecodedLine,
+                                         'note': rawLineNotes});
     }
     // end for rawlines
-    if (this.lastDecodedStanza.length != 0) {
+    if (this.lastDecodedStanza.lines.length != 0) {
       // quedó una estrofa sin pushear
+      this.lastDecodedStanza.name = this.nextStanzaName;
       this.decodedSong.push(this.lastDecodedStanza);
     }
     this.isDecoded = true;
   };
 
+  this.parseChordSequence = function (rawChordSequence) {
+    return rawChordSequence.substr(1).split('|').map((chordMora) =>
+      // ignorar todo después del primer '-' que sucede al acorde
+      chordMora.replace(/^-+/g,'').replace(/-.*/g,'').replace(/ +/g, '')
+    )
+  }
+
   this.getLastDecodedLine = function() {
-    let ldsLen = this.lastDecodedStanza.length;
+    let ldsLen = this.lastDecodedStanza.lines.length;
     if (ldsLen != 0) {
-      return this.lastDecodedStanza[ldsLen - 1];
+      return this.lastDecodedStanza.lines[ldsLen - 1];
     }
 
     let songLen = this.decodedSong.length;
     if (songLen != 0) {
-      let lds = this.decodedSong[songLen - 1];
-      return lds[lds.length - 1];
+      let ldStanza = this.decodedSong[songLen - 1];
+      return ldStanza.lines[ldStanza.lines.length - 1];
     }
 
     return [];
@@ -318,7 +397,7 @@ function LChorder(data, target, options) {
       this.decode();
     }
 
-    this.chordise(node);
+    this.chordise(this.target);
     let chordRows = document.getElementsByClassName('lch-chords');
     for (let i=0; i<chordRows.length; i++) {
       let chordRow = chordRows[i];
